da5c383cbb4f90f7c93321f2f3405cc343fc0e96
Rohit Chauhan <nixenboi@gmail.com>
2026-04-23 12:18:09 +0530

    navbar

---
+++ components/Header/Nav/data.ts
@@ -0,0 +1,19 @@
+export interface NavLink {
+  title: string;
+  href: string;
+}
+
+export const links: NavLink[] = [
+  { title: "Venues", href: "/" },
+  { title: "Sports", href: "/" },
+  { title: "Events", href: "/" },
+  { title: "Coaching", href: "/" },
+  { title: "Partner", href: "/" },
+];
+
+export const footerLinks: NavLink[] = [
+  { title: "Facebook", href: "/" },
+  { title: "LinkedIn", href: "/" },
+  { title: "Instagram", href: "/" },
+  { title: "Twitter", href: "/" },
+];

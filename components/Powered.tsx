2531040df52c20020eff4fc66723a3d49a1f71aa
akashnegi62 <akashsing215@gmail.com>
2026-05-04 17:55:04 +0530

    logo

--- components/Powered.tsx
+++ components/Powered.tsx
@@ -7,7 +7,7 @@ const Powered = () => {
   return (
     <section className="bg-(--white-bg) p-6 md:p-12 font-sans">
       <div className="max-w-6xl mx-auto space-y-8">
-        <h2 className="text-4xl text-center md:text-5xl font-bold mb-6 leading-tight">
+        <h2 className="text-(--white-text) text-4xl text-center md:text-5xl font-bold mb-6 leading-tight">
           Powered by Callories
         </h2>
 
@@ -15,7 +15,7 @@ const Powered = () => {
         <div className="bg-(--dark-bg)/10 rounded-[40px] overflow-hidden flex flex-col md:flex-row items-center p-4 md:p-8 border-2 border-(--highlight)/10">
           {/* Image Container */}
           <div className="w-full md:w-1/2 relative h-[300px] md:h-[400px] rounded-[30px] overflow-hidden">
-            <div className="absolute inset-0 bg-zinc-800 flex items-center justify-center text-zinc-500">
+            <div className="absolute inset-0 flex items-center justify-center text-zinc-500">
               {/* Replace with your actual image path */}
               <Image
                 src="/Img/Powered.jpg"
@@ -23,7 +23,7 @@ const Powered = () => {
                 loading="eager"
                 width={600}
                 height={600}
-                className="object-contain w-full h-full"
+                className="object-cover w-full h-full"
               />
             </div>
           </div>
@@ -41,7 +41,7 @@ const Powered = () => {
             <Link
               target="_blank"
               href="https://callories.vercel.app/"
-              className="inline-flex items-center justify-between bg-(--highlight) text-black px-6 py-4 rounded-full font-bold w-fit hover:bg-white transition-colors group"
+              className="inline-flex items-center justify-between bg-(--highlight) text-(--dark-bg) px-6 py-4 rounded-full font-bold w-fit hover:bg-white transition-colors group"
             >
               Explore Callories
               <div className="ml-4 rounded-full p-1 ">
@@ -54,7 +54,7 @@ const Powered = () => {
         {/* --- Final CTA Section --- */}
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6 pt-8">
           <div className="flex flex-col justify-center">
-            <h3 className="text-4xl md:text-5xl font-black text-black leading-tight uppercase italic">
+            <h3 className="text-4xl md:text-5xl text-(--white-text) font-black leading-tight uppercase italic">
               Don’t Just <br /> Watch Sports. <br /> Be Part of It.
             </h3>
           </div>
@@ -63,14 +63,14 @@ const Powered = () => {
             <Link
               target="_blank"
               href="https://callories.vercel.app/"
-              className="w-full sm:w-auto text-center bg-black text-white px-10 py-5 rounded-2xl font-bold text-lg hover:scale-105 transition-transform"
+              className="w-full sm:w-auto text-center bg-(--dark-bg) text-white px-10 py-5 rounded-2xl font-bold text-lg hover:scale-105 transition-transform"
             >
               Book Your Slot
             </Link>
             <Link
               target="_blank"
               href="https://callories.vercel.app/"
-              className="w-full sm:w-auto text-center border-2 border-black text-black px-10 py-5 rounded-2xl font-bold text-lg hover:bg-black hover:text-white transition-all"
+              className="w-full sm:w-auto text-center border-2 border-(--dark-bg) text-(--dark-bg) px-10 py-5 rounded-2xl font-bold text-lg hover:bg-(--dark-bg) hover:text-white transition-all"
             >
               Partner With Us
             </Link>
